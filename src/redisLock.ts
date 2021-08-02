e99679b78da4c0a9b151e23393093d37a09f6def
zcong1993 <zhangcong1992@gmail.com>
2021-08-02 11:44:26 +0800

    feat: redisLock del use lua keep it atomic

--- src/redisLock.ts
+++ src/redisLock.ts
@@ -1,6 +1,30 @@
 import type { Redis } from 'ioredis'
 import { randString } from './string'
 
+const delCommandKey = Symbol.for('node-kit delCommandKey')
+
+const addDelCommand = (redis: Redis) => {
+  if ((redis as any)[delCommandKey]) {
+    return
+  }
+
+  ;(redis as any)[delCommandKey] = true
+
+  redis.defineCommand('tryLockSafeDel', {
+    numberOfKeys: 1,
+    lua: `if redis.call("GET",KEYS[1]) == ARGV[1]
+    then
+        return redis.call("DEL",KEYS[1])
+    else
+        return 0
+    end`,
+  })
+}
+
+interface ExtRedis extends Redis {
+  tryLockSafeDel(key: string, compareVal: string): Promise<any>
+}
+
 export type UnLockFn = () => Promise<void>
 
 /**
@@ -16,6 +40,8 @@ export const tryLock = async (
   key: string,
   px: number
 ): Promise<[boolean, UnLockFn]> => {
+  addDelCommand(redis)
+
   const val = `${Date.now()}-${randString(10)}`
   const lock = await redis.set(key, val, 'nx', 'px', px)
 
@@ -25,9 +51,7 @@ export const tryLock = async (
   if (lock) {
     // only unlock self lock
     unlockFn = async () => {
-      if ((await redis.get(key)) === val) {
-        await redis.del(key)
-      }
+      await (redis as ExtRedis).tryLockSafeDel(key, val)
     }
   }
 
