66e6ce05e215f840ac13d1802a4145f75fee9777
zcong1993 <zhangcong1992@gmail.com>
2021-10-14 19:24:20 +0800

    feat(crypto): add golang compatable aes gcm

---
+++ __test__/crypto/aes.test.ts
@@ -0,0 +1,72 @@
+import { randomBytes } from 'crypto'
+import { randRangeInt, randString } from '../../src'
+import { AesGcm, AesGcmType } from '../../src/crypto/aes'
+import { intRange } from '../testUtils'
+
+describe.each([128, 192, 256])('aes-%d-gcm', (t: AesGcmType) => {
+  const keyLen = t / 8
+  const ac = new AesGcm(t)
+  it('should works', () => {
+    intRange(100).forEach(() => {
+      const key = randString(keyLen)
+      const testBuf = randomBytes(randRangeInt(1, 1000))
+      const enc = ac.encrypt(testBuf, key)
+      const dec = ac.decrypt(enc, key)
+      expect(dec.equals(testBuf)).toBeTruthy()
+    })
+  })
+})
+
+describe('invald type', () => {
+  it('should throw', () => {
+    expect(() => new AesGcm(100 as any)).toThrow()
+  })
+})
+
+describe('test decrypt encrypted data from golang', () => {
+  const testData = 'hello2222-cdscsdcsd'
+  it('aes-256-gcm', () => {
+    const ac = new AesGcm(256)
+    expect(
+      ac
+        .decrypt(
+          Buffer.from(
+            '9d2cdd1c45bd6d1f360ec7815a6d5fb98a71d37a6a91bf6f6f9183b0d00b1aba0b0f1ee5c09bff0e3ec1e72b6468a0',
+            'hex'
+          ),
+          '0'.repeat(32)
+        )
+        .toString()
+    ).toBe(testData)
+  })
+
+  it('aes-192-gcm', () => {
+    const ac = new AesGcm(192)
+    expect(
+      ac
+        .decrypt(
+          Buffer.from(
+            '70c8dbf48e7979572d834adeb01201ae3e4a247e81d7c84d29cd1728b2723836db635cde9a5aff748c277263f5eae7',
+            'hex'
+          ),
+          '0'.repeat(24)
+        )
+        .toString()
+    ).toBe(testData)
+  })
+
+  it('aes-128-gcm', () => {
+    const ac = new AesGcm(128)
+    expect(
+      ac
+        .decrypt(
+          Buffer.from(
+            '185ef28d6c6624144cb01d60dcaa8029df3c997f3cdae16e109a0af276d1896bd028ffbccb80283736568f02450cdc',
+            'hex'
+          ),
+          '0'.repeat(16)
+        )
+        .toString()
+    ).toBe(testData)
+  })
+})
---
+++ src/crypto/aes.ts
@@ -0,0 +1,55 @@
+import {
+  createCipheriv,
+  createDecipheriv,
+  randomBytes,
+  BinaryLike,
+  CipherGCMTypes,
+} from 'crypto'
+
+export type AesGcmType = 128 | 192 | 256
+
+// AesGcm compact with golang
+export class AesGcm {
+  static authTagLength = 16
+  static ivLength = 12
+  private algorithm: CipherGCMTypes
+  constructor(readonly type: AesGcmType) {
+    if (![128, 192, 256].includes(type)) {
+      throw new Error('aes gcm type should be one of 128, 192, 256')
+    }
+
+    this.algorithm = `aes-${this.type}-gcm`
+  }
+
+  /**
+   * encrypt a text string or buffer
+   * @param plaintext - text string of buffer to encrypt
+   * @param key - encryption secret key
+   * @returns [iv:12][encryptedString][authTag:16]
+   */
+  encrypt(plaintext: BinaryLike, key: string) {
+    const iv = randomBytes(AesGcm.ivLength)
+    const cipher = createCipheriv(this.algorithm, key, iv)
+    const encryptedBuffer = Buffer.concat([
+      cipher.update(plaintext),
+      cipher.final(),
+    ])
+    const authTag = cipher.getAuthTag()
+    return Buffer.concat([iv, encryptedBuffer, authTag])
+  }
+
+  /**
+   * decrypt
+   * @param encrypted - encrypted text
+   * @param key - encryption secret key
+   * @returns Buffer
+   */
+  decrypt(encrypted: Buffer, key: string) {
+    const iv = encrypted.slice(0, 12)
+    const authTag = encrypted.slice(encrypted.length - 16)
+    const encryptedText = encrypted.slice(12, encrypted.length - 16)
+    const decipher = createDecipheriv(this.algorithm, key, iv)
+    decipher.setAuthTag(authTag)
+    return Buffer.concat([decipher.update(encryptedText), decipher.final()])
+  }
+}
