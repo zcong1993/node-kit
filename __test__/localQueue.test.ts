ffc116166ef16a81efcca7949530bcd98c4c9b0b
zcong1993 <zhangcong1992@gmail.com>
2022-05-02 12:24:33 +0800

    chore: fix deperated callback use promise

--- __test__/localQueue.test.ts
+++ __test__/localQueue.test.ts
@@ -2,82 +2,93 @@ import { LocalQueue, LocalQueueError } from '../src/localQueue'
 import { delayFn, intRange } from './testUtils'
 
 describe('LocalQueue', () => {
-  it('should works', (done) => {
+  it('should works', async () => {
     const res: any[] = []
     const q = new LocalQueue<number>('test1')
 
-    q.startProcess(async (data, task) => {
-      await delayFn(10, null)()
-      res.push([data, task])
-    }).then(() => {
-      const expected = intRange(10).map((x) => [x, { data: x }])
-      expect(res).toStrictEqual(expected)
-      expect(q.processed).toBe(10)
-      done()
-    })
+    await new Promise((r) => {
+      q.startProcess(async (data, task) => {
+        await delayFn(10, null)()
+        res.push([data, task])
+      }).then(() => {
+        const expected = intRange(10).map((x) => [x, { data: x }])
+        expect(res).toStrictEqual(expected)
+        expect(q.processed).toBe(10)
+        r(null)
+      })
 
-    intRange(10).map((x) => q.push(x))
-    q.complete()
+      intRange(10).map((x) => q.push(x))
+      q.complete()
+    })
   })
 
-  it('push before startProcess', (done) => {
+  it('push before startProcess', async () => {
     const res: number[] = []
     const q = new LocalQueue<number>('test1')
     q.push(1)
 
-    q.startProcess(async (data) => {
-      res.push(data)
-    }).then(() => {
-      expect(res).toStrictEqual([1, ...intRange(10)])
-      done()
-    })
+    await new Promise((r) => {
+      q.startProcess(async (data) => {
+        res.push(data)
+      }).then(() => {
+        expect(res).toStrictEqual([1, ...intRange(10)])
+        r(null)
+      })
 
-    intRange(10).map((x) => q.push(x))
-    q.complete()
+      intRange(10).map((x) => q.push(x))
+      q.complete()
+    })
   })
 
-  it('complete before startProcess will process nothing', (done) => {
+  it('complete before startProcess will process nothing', async () => {
     const res: number[] = []
     const q = new LocalQueue<number>('test1')
-    q.push(1)
-    q.push(2)
-    q.complete()
 
-    q.startProcess(async (data) => {
-      res.push(data)
-    }).then(() => {
-      expect(res).toStrictEqual([])
-      done()
+    await new Promise((r) => {
+      q.push(1)
+      q.push(2)
+      q.complete()
+
+      q.startProcess(async (data) => {
+        res.push(data)
+      }).then(() => {
+        expect(res).toStrictEqual([])
+        r(null)
+      })
+
+      intRange(10).map((x) => q.push(x))
     })
-
-    intRange(10).map((x) => q.push(x))
   })
 
-  it('onError', (done) => {
+  it('onError', async () => {
     const onError = vi.fn()
     const q = new LocalQueue<number>('test1', 10, onError)
-    q.startProcess(async (data) => {
-      if (data < 5) {
-        throw new Error()
-      }
-      await delayFn(10, null)()
-    }).then(() => {
-      expect(onError).toBeCalledTimes(5)
-      expect(onError.mock.calls[0][0]).toBeInstanceOf(LocalQueueError)
-      done()
-    })
+    await new Promise((r) => {
+      q.startProcess(async (data) => {
+        if (data < 5) {
+          throw new Error()
+        }
+        await delayFn(10, null)()
+      }).then(() => {
+        expect(onError).toBeCalledTimes(5)
+        expect(onError.mock.calls[0][0]).toBeInstanceOf(LocalQueueError)
+        r(null)
+      })
 
-    intRange(10).map((x) => q.push(x))
-    q.complete()
+      intRange(10).map((x) => q.push(x))
+      q.complete()
+    })
   })
 
-  it('concurrent', (done) => {
+  it('concurrent', async () => {
     const q = new LocalQueue<number>('test1', 2)
-    q.startProcess(async () => {
-      await delayFn(10, null)()
-    }).then(done)
+    await new Promise((r) => {
+      q.startProcess(async () => {
+        await delayFn(10, null)()
+      }).then(() => r(null))
 
-    intRange(10).map((x) => q.push(x))
-    q.complete()
+      intRange(10).map((x) => q.push(x))
+      q.complete()
+    })
   })
 })
